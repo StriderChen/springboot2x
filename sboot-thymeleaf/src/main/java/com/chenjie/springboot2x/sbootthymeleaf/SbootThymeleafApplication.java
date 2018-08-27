12cf0b5223aada7d054a36c744a3b8746110b881
myGit <729217667@qq.com>
2018-08-28 00:41:58 +0800

    springboot2.x整合thymeleaf模板视图

---
+++ sboot-thymeleaf/src/main/java/com/chenjie/springboot2x/sbootthymeleaf/SbootThymeleafApplication.java
@@ -0,0 +1,16 @@
+package com.chenjie.springboot2x.sbootthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenjie
+ * @date 2018/08/27
+ * @description SpringBoot之使用Thymeleaf视图模板
+ */
+@SpringBootApplication
+public class SbootThymeleafApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SbootThymeleafApplication.class, args);
+    }
+}
