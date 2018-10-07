61bc685394ac4f05aa177dd9c5a8b62077c92a00
myGit <729217667@qq.com>
2018-10-08 00:06:29 +0800

    整合jms实现了发送邮件的功能

---
+++ sboot-jms/src/main/java/com/chenjie/sbootjms/SbootJmsApplication.java
@@ -0,0 +1,12 @@
+package com.chenjie.sbootjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbootJmsApplication.class, args);
+    }
+}
