106671db35ac48ff3c72936f6e8c33700bcb286d
myGit <729217667@qq.com>
2018-10-08 22:07:49 +0800

    springboot整合rabbitmq,未完待续

---
+++ sboot-rabbitmq/consumer/src/main/java/com/chenjie/sbootrabbitmq/consumer/ConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.chenjie.sbootrabbitmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author chenjie
+ * @Date 2018/9/13 10:39
+ * @Description: springboot2.x整合rabbitmq消息中间件--consumer
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class);
+    }
+}
---
+++ sboot-rabbitmq/consumer/src/test/java/com/chenjie/sbootrabbitmq/consumer/ConsumerTest.java
@@ -0,0 +1,15 @@
+package com.chenjie.sbootrabbitmq.consumer;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author chenjie
+ * @Date 2018/9/13 10:39
+ * @Description:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsumerTest {
+}
